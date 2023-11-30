fcf56bf6ace6c691ffde0b9bd897d43de94dcca2
pedromonteiro01 <pmapm@ua.pt>
2023-11-30 15:55:04 +0000

    persist darkmode preferences

--- frontend/src/components/DarkModeContext/DarkModeContext.js
+++ frontend/src/components/DarkModeContext/DarkModeContext.js
@@ -1,12 +1,21 @@
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useContext, useEffect } from 'react';
 
 const DarkModeContext = createContext();
 
 export const useDarkMode = () => useContext(DarkModeContext);
 
 export const DarkModeProvider = ({ children }) => {
-    const [darkMode, setDarkMode] = useState(false);
-    
+    // Initialize darkMode from local storage, or default to false
+    const [darkMode, setDarkMode] = useState(() => {
+        const savedDarkMode = localStorage.getItem('darkMode');
+        return savedDarkMode !== null ? savedDarkMode === 'true' : false;
+    });
+
+    // Update local storage whenever darkMode changes
+    useEffect(() => {
+        localStorage.setItem('darkMode', darkMode);
+    }, [darkMode]);
+
     return (
         <DarkModeContext.Provider value={{ darkMode, setDarkMode }}>
             {children}
